cbc9f047de51d9eef073eed9562f59a8969b8940
Arnav Ram <169087658+ArnavRam@users.noreply.github.com>
2025-10-01 20:09:05 +0530

    Add files via upload

---
+++ Constructor.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    int age;
+    int roll_no;
+public:
+    Student(string n, int a,int r) {
+        name = n;
+        age = a;
+        roll_no= r;
+    }
+
+    Student(const Student &s) {
+        name = s.name;
+        age  = s.age;
+        roll_no = s.roll_no;
+        cout << "Copy constructor called!\n";
+    }
+
+    void display() {
+        cout << "Name: " << name << ", Age: " << age << ", Roll Number: " << roll_no << endl;
+    }
+};
+
+int main() {
+    Student s1("Arnav", 19, 503);
+    cout << "Original object:\n";
+    s1.display();
+
+    Student s2 = s1;
+    cout << "\nCopied object:\n";
+    s2.display();
+
+    return 0;
+}
+
+// OUTPUT
+// Original object:
+// Name: Arnav, Age: 19, Roll Number: 503
+// Copy constructor called!
+
+// Copied object:
+// Name: Arnav, Age: 19, Roll Number: 503
---
+++ Constructor_inside_the_Class.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    double fee;
+
+public:
+    Student() {
+        cout << "Enter the name of Student: ";
+        cin >> name;
+        cout << "Enter the fee: ";
+        cin >> fee;
+    }
+
+    void display() {
+        cout << "\nStudent Name: " << name;
+        cout << "\nFee: " << fee << endl;
+    }
+};
+
+int main() {
+    Student s;
+    s.display();
+    return 0;
+}
+
+// OUTPUT
+// Enter the name of Student: Arnav
+// Enter the fee: 312000
